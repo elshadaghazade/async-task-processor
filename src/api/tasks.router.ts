4f1456e775eb69af2b26b7b78f6807c67aa508f0
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-04-01 20:56:17 +0200

    added unimplemented task post router and request body validator using zod

---
+++ src/api/tasks.router.ts
@@ -0,0 +1,7 @@
+import { Router, type Request, type Response } from 'express';
+
+export const tasksRouter = Router();
+
+tasksRouter.post('/', async (req: Request, res: Response) => {
+  
+});
---
+++ src/api/validators/submit-task.validator.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const SubmitTaskSchema = z.object({
+    taskId: z.string().regex(/^[a-zA-Z0-9_-]+$/, {
+        message: 'taskId must be alphanumeric',
+    }).optional(),
+    payload: z.record(z.string(), z.unknown())
+});
