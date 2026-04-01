24ef008f0a977a67e8fadf0eb02a92e2cefdfc67
Elshad Aghayev <3605068+elshadaghazade@users.noreply.github.com>
2026-04-01 21:32:22 +0200

    enqueueTask was used in the task post endpoint

--- src/api/tasks.router.ts
+++ src/api/tasks.router.ts
@@ -10,44 +10,44 @@ import { TaskStatusEnum } from '../generated/prisma';
 export const tasksRouter = Router();
 
 tasksRouter.post('/', async (req: Request, res: Response) => {
-  const parsed = SubmitTaskSchema.safeParse(req.body);
+    const parsed = SubmitTaskSchema.safeParse(req.body);
 
-  if (!parsed.success) {
-    return res.status(400).json({
-      error: 'Validation failed',
-      details: flattenError(parsed.error),
+    if (!parsed.success) {
+        return res.status(400).json({
+            error: 'Validation failed',
+            details: flattenError(parsed.error),
+        });
+    }
+
+    const { taskId, payload } = parsed.data;
+
+    let _taskId = taskId;
+
+    if (!_taskId) {
+        _taskId = randomUUID();
+    }
+
+    const existing = await prisma.task.findUnique({ where: { taskId: _taskId } });
+
+    if (existing) {
+        logger.info({ taskId: _taskId }, 'Task already exists - returning current state');
+
+        return res.status(200).json({
+            message: 'Task already submitted',
+            task: existing,
+        });
+    }
+
+    const task = await prisma.task.create({
+        data: { taskId: _taskId, payload: payload as any, status: TaskStatusEnum.PENDING },
     });
-  }
 
-  const { taskId, payload } = parsed.data;
+    await enqueueTask({ taskId: _taskId, payload });
 
-  let _taskId = taskId;
+    logger.info({ taskId: _taskId }, 'Task submitted and enqueued');
 
-  if (!_taskId) {
-    _taskId = randomUUID();
-  }
-
-  const existing = await prisma.task.findUnique({ where: { taskId: _taskId } });
-
-  if (existing) {
-    logger.info({ taskId: _taskId }, 'Task already exists - returning current state');
-
-    return res.status(200).json({
-      message: 'Task already submitted',
-      task: existing,
+    return res.status(202).json({
+        message: 'Task accepted',
+        task,
     });
-  }
-
-  const task = await prisma.task.create({
-    data: { taskId: _taskId, payload: payload as any, status: TaskStatusEnum.PENDING },
-  });
-
-  await enqueueTask({ taskId: _taskId, payload });
-
-  logger.info({ taskId: _taskId }, 'Task submitted and enqueued');
-
-  return res.status(202).json({
-    message: 'Task accepted',
-    task,
-  });
 });
